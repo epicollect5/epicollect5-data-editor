1da8f690c45aa381f57c0f22fed5b538a26a32b5
mirko77 <mirko.menegazzo@gmail.com>
2024-04-05 18:29:15 +0200

    Reverted changes, will do it directly on the new PWA

--- src/components/SaveEntry.js
+++ src/components/SaveEntry.js
@@ -7,21 +7,6 @@ import strings from '../constants/strings';
 
 const SaveEntry = ({ saveEntry, isEdit, entry, branchEntries }) => {
 
-    let showBranchesToBeDeletedWarning = false;
-
-    //if a hierarchy edit and some existing branches will be deleted, show warning
-    if (isEdit) {
-        Object.keys(branchEntries).forEach((ownerInputRef) => {
-            //if the ownerInputRef was jumped,this will cause the existing
-            //leftover branches to be deleted server side
-            if (entry.entry.answers[ownerInputRef].was_jumped === true) {
-                //show warning
-                showBranchesToBeDeletedWarning = true;
-                return false;
-            }
-        });
-    }
-
     return (
         <div className="ec5-view text-center">
             <button
@@ -30,14 +15,6 @@ const SaveEntry = ({ saveEntry, isEdit, entry, branchEntries }) => {
             >
                 {strings.save_entry}
             </button>
-            {showBranchesToBeDeletedWarning
-                ? <p className="ec5-notice">
-                    Please be advised that by opting to save,
-                    it will trigger the removal of certain existing BRANCH entries
-                    as a result of the current conditional flow (JUMPS).
-                </p>
-                : null
-            }
         </div>
     );
 };
